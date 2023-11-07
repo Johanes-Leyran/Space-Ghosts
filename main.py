2f09f30bcbb7f5767d82d27f68b9a3bc63531d4b
Johanes-Leyran <102525247+Johanes-Leyran@users.noreply.github.com>
2023-11-07 23:15:03 +0800

    organize code
    put the user key handling inside the player class
    
    rename from Space Raider to space ghost

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-from game import Background, Player
+from entity import Background, Player
+import time
 import pygame
 pygame.init()
 
@@ -10,13 +11,11 @@ scroll_speed = 2
 background_color = (0, 0, 0)
 
 screen = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT), pygame.HWSURFACE | pygame.DOUBLEBUF)
-pygame.display.set_caption('Space Raiders')
+pygame.display.set_caption('Space Ghost')
 
 clock = pygame.time.Clock()
-prev_time = pygame.time.get_ticks()
+prev_time = time.time()
 
-# ToDo change the list into pygame group
-background_group = pygame.sprite.Group()
 background_1 = Background(scroll_speed, 0, 0)
 background_2 = Background(scroll_speed, 0, 0)
 
@@ -24,16 +23,20 @@ background_2 = Background(scroll_speed, 0, 0)
 background_1.rect.y = -(background_1.image.get_height() - WINDOW_HEIGHT)
 background_2.rect.y = -(background_2.image.get_height() + background_1.image.get_height() - WINDOW_HEIGHT)
 
-background_group.add(background_1, background_2)
+background_group = pygame.sprite.Group(background_1, background_2)
 
-running = True
+player = Player(WINDOW_WIDTH/2, WINDOW_HEIGHT/2, 10)
 
-player = Player(WINDOW_WIDTH/2, WINDOW_HEIGHT/2)
+# makes the cursor disappear when playing the game
+# pygame.mouse.set_cursor((8,8),(0,0),(0,0,0,0,0,0,0,0),(0,0,0,0,0,0,0,0))
 
 
 def handle_background():
     background_group.update()
     background_group.draw(screen)
+
+
+running = True
 
 
 while running:
@@ -42,23 +45,10 @@ while running:
             running = False
 
     # ToDo organize handling the key pressed
-    keys = pygame.key.get_pressed()
-    if keys[pygame.K_LEFT]:
-        player.position[0] -= 10
-    if keys[pygame.K_RIGHT]:
-        player.position[0] += 10
-    if keys[pygame.K_UP]:
-        player.position[1] -= 5
-    if keys[pygame.K_DOWN]:
-        player.position[1] += 5
-
-    # Todo implement proper delta time
-    current_time = pygame.time.get_ticks()
-    delta_time = (current_time - prev_time) / 1000.0
+    player.handle_key()
 
     screen.fill((100, 100, 100))
     handle_background()
-    player.draw(screen)
 
     clock.tick(FPS)
     pygame.display.flip()
